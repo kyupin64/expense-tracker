3b462a4e69ce5f2f730a34fb2e5006bdc9fbcc71
kyupin64 <lewilks.321@gmail.com>
2024-04-26 18:42:58 -0600

    Add keys to menu options

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,16 @@
 export default function Menu({ setShowMenu }) {
-    const listPlaceholder = ["item1", "item2", "item3"];
+    const listPlaceholder = [
+        {id: "item1", value: "item1"},
+        {id: "item2", value: "item2"},
+        {id: "item3", value: "item3"},
+    ];
 
     return (
         <div className="px-6 pb-6 h-full w-full flex justify-center items-center">
             <div className="p-10 h-full w-full flex flex-col border-4 border-red-500 rounded-lg">
-                <label for="menu-list">Have a list</label>
+                <label htmlFor="menu-list">Have a list</label>
                 <select id="menu-list" name="menu-list">
-                    {listPlaceholder.map((item) => <option value={item} onClick={() => setShowMenu(false)}>{item}</option>)}
+                    {listPlaceholder.map((item) => <option key={item.id} value={item} onClick={() => setShowMenu(false)}>{item.value}</option>)}
                 </select>
             </div>
         </div>
